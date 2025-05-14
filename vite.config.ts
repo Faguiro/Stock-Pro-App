a9c5ce0eb6884f56c12837127f5f909ab8cbf989
Faguiro <Faguiro2005@gmail.com>
2025-05-14 15:33:19 -0300

    Add proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://3.148.189.38:8000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
+
